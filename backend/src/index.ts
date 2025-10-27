f18c99cd121770bb6c19371ee0cfa21fafe5684d
Valentin <me@valentingaillard.com>
2025-10-27 20:22:21 +0100

    feat(backend): load data from JSON file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,20 @@
 import { app } from './app'
+import * as fs from 'fs'
 
 const PORT = 5555
+
+// Load json data
+function loadJsonData(path: string) {
+  const data = fs.readFileSync(path, 'utf-8')
+  return JSON.parse(data)
+}
+
+try {
+  const jsonData = loadJsonData('./public/data.json')
+  console.log('JSON data loaded successfully')
+} catch (error) {
+  console.error('Error loading JSON data:', error)
+}
 
 // Start the server
 app
